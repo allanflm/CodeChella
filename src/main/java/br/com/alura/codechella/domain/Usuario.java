66ae12bef5d1615da0ce130698f325e046987f62
Allan <allanfelipedk@gmail.com>
2024-04-12 19:45:34 -0300

    getting to know the project

---
+++ src/main/java/br/com/alura/codechella/domain/Usuario.java
@@ -0,0 +1,44 @@
+package br.com.alura.codechella.domain;
+
+import java.time.LocalDate;
+
+
+public class Usuario {
+    private String cpf;
+    private String nome;
+    private LocalDate nascimento;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    private String email;
+}
