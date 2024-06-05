9374f4787f7b83f9bcd3f8f28175bf538dcdad8a
Allan <allanfelipedk@gmail.com>
2024-06-05 07:44:06 -0300

    feat: implementing a list

--- src/main/java/br/com/alura/codechella/CodechellaApplication.java
+++ src/main/java/br/com/alura/codechella/CodechellaApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CodechellaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CodechellaApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CodechellaApplication.class, args);
+	}
+
 }
--- src/main/java/br/com/alura/codechella/infra/controller/UserDTO.java
+++
@@ -1,9 +0,0 @@
-package br.com.alura.codechella.infra.controller;
-
-import java.time.LocalDate;
-
-public record UserDTO(String name,
-                      String cpf,
-                      LocalDate birth, // Data de nascimento
-                      String email) {
-}
